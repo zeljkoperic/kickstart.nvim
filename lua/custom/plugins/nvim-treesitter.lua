556c86448160ecb0156f1c3f0397ee2c9a11665b
zeljko <zeljko.zona@gmail.com>
2024-05-20 01:43:03 +0200

    treesitter-text-objects

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -1,6 +1,8 @@
 return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
   opts = {
     ensure_installed = {
       "bash",
@@ -9,6 +11,9 @@ return { -- Highlight, edit, and navigate code
       "lua",
       "luadoc",
       "markdown",
+      "markdown_inline",
+      "dockerfile",
+      "gitignore",
       "vim",
       "vimdoc",
       "json",
@@ -22,27 +27,30 @@ return { -- Highlight, edit, and navigate code
       "python",
       "rust",
       "go",
+      "sql",
     },
-    -- Autoinstall languages that are not installed
+
     auto_install = true,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { "ruby" },
     },
     indent = { enable = true, disable = { "ruby" } },
   },
   config = function(_, opts)
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-    -- Prefer git instead of curl in order to improve connectivity in some environments
     require("nvim-treesitter.install").prefer_git = true
     ---@diagnostic disable-next-line: missing-fields
     require("nvim-treesitter.configs").setup(opts)
-    -- use autotag for auto closing tags
     require("nvim-treesitter.configs").setup {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
       autotag = {
         enable = true,
       },
@@ -51,11 +59,5 @@ return { -- Highlight, edit, and navigate code
         enable = true,
       },
     }
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   end,
 }
